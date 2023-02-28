5a39af26aaee160760cac338f0d324fc4cfc49d4
rayanbopp <portfolio.it.zh@gmail.com>
2023-02-28 15:06:57 +0100

    added

---
+++ modules/m319/Tag2/JavaAssignments2/AssignmentJava2.java
@@ -0,0 +1,15 @@
+package m319.Tag2.JavaAssignments2;
+
+/**
+ * AssignmentJava2
+ */
+public class AssignmentJava2 {
+
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+            System.out.println(sum);
+        }
+    }
+}
